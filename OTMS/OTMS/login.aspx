7b3384bb91d1bdf3f73e8d4017cf6c0b32254882
mehul <mehulbudasna23@gmail.com>
2016-11-07 10:50:11 +0530

    update

--- OTMS/OTMS/login.aspx
+++ OTMS/OTMS/login.aspx
@@ -1,16 +1,61 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="OTMS.login" %>
 
-<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-    <div>
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Adm</title>
+    <style type="text/css">
+	body{
+	 background-image: url("21.jpg");
+	}
+	</style>
     
-    </div>
-    </form>
-</body>
+    
+    <link rel='stylesheet prefetch' href="css/sty2.css">
+
+        <link rel="stylesheet" href="css/style.css">
+
+    
+    
+    
+  </head>
+<script src="js/jquery.min.js"></script>
+
+        <script src="js/index.js"></script>
+
+    
+  <body>
+
+      <form id="form1" runat="server">
+
+    <div class="login-form">
+     <h1>Login</h1>
+     <div class="form-group ">
+       <input type="text" class="form-control" placeholder="Username " id="UserName">
+       <i class="fa fa-user"></i>
+     </div>
+     <div class="form-group log-status">
+       <input type="password" class="form-control" placeholder="Password" id="Password"><br />
+         <br />
+         <asp:Label ID="error_msg" runat="server"></asp:Label>
+&nbsp;<i class="fa fa-lock"></i><span class="alert">Invalid Credentials</span>
+      <a class="link" href="#">Lost your password?</a>
+     <button type="button" class="log-btn" runat="server"  ID="submit_btn" OnClick="submit_btn_Click">Log in</button>
+     
+
+         
+    
+   </div>
+    
+     
+    
+    
+      </form>
+    
+     
+    
+    
+  </body>
 </html>
--- OTMS/OTMS/login.aspx.cs
+++ OTMS/OTMS/login.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,11 +8,67 @@ using System.Web.UI.WebControls;
 
 namespace OTMS
 {
-	public partial class login : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        int flag;
+        String conform_str, dropdown_str;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void submit_btn_Click(Object sender, EventArgs e)
+        {
+            
 
-		}
-	}
+
+            String usernm = Username.Text;
+            String passwd = Password.Text;
+            String connstring = "Data Source=ABCD;initial catalog=OTMS;integrated security=true";
+
+            SqlConnection conn = new SqlConnection(connstring);
+            try
+            {
+                String s = "";
+                conn.Open();
+                SqlCommand command = new SqlCommand();
+                
+                String selectdata = "select * from login_details ;
+                command.Connection = conn;
+                command.CommandText = selectdata;
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if ((reader["username"].Equals(usernm) && reader["password"].Equals(passwd)))
+                    {
+                        //if (reader["type"].Equals(conform_str))
+                        // {
+
+                        flag = 1;
+                        
+                            Session["username"] = Username.Text;
+                            Session["password"] = Password.Text;
+                            Response.Redirect("homepage.aspx");
+                        //}
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                if (flag != 1)
+                {
+                    error_msg.Text = "Invalid Username or Password";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //throw ex;
+            }
+        }
+    }
+
 }
